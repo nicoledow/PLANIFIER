8b572f7aef60c69439c9ced1f65a58c4f87388d3
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 18:01:52 -0400

    updated lessons show_all route

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
 
     get '/lessons' do 
       @teacher = Teacher.find_by_id(session[:user_id])
+      @teacher_first_name = @teacher.name.split(" ").first
       erb :'/lessons/show_all'
     end
 
