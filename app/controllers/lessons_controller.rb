3fad2f6e695fbaeaf268da8ab8e6d5d52056ae0d
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 18:18:31 -0400

    added validation that user is logged in to see /lessons page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,18 @@
 class LessonsController < ApplicationController
 
     get '/lessons' do 
-      @teacher = Teacher.find_by_id(session[:user_id])
-      @teacher_first_name = @teacher.name.split(" ").first
-      @lessons = Lesson.all
-      erb :'/lessons/show_all'
+      if logged_in?
+        @teacher = Teacher.find_by_id(session[:user_id])
+        @teacher_first_name = @teacher.name.split(" ").first
+        @lessons = Lesson.all
+        erb :'/lessons/show_all'
+      else
+        redirect to '/login'
+      end
+    end
+
+    get '/lessons/new' do
+
     end
 
 end
