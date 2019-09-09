e4ff78ed55a7ab06b6d04f09dc374405e3c3de15
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 14:51:33 -0400

    udated tables (not migrated yet)

--- db/migrate/20190909184154_create_lessons.rb
+++ db/migrate/20190909184154_create_lessons.rb
@@ -1,7 +1,11 @@
 class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
-
+      t.string :title
+      t.string :objectives
+      t.string :content
+      t.string :assessment
+      t.integer :course_id
     end
   end
 end
