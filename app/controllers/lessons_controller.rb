0952e68440ecef386c9d965256b9e3099c9d3d27
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 18:15:49 -0400

    added code to show list of all lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,7 @@ class LessonsController < ApplicationController
     get '/lessons' do 
       @teacher = Teacher.find_by_id(session[:user_id])
       @teacher_first_name = @teacher.name.split(" ").first
+      @lessons = Lesson.all
       erb :'/lessons/show_all'
     end
 
