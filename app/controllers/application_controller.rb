f21dc8c79027be38588cc747bd88a329bad5cd6f
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 17:34:47 -0400

    added instantiation route for new teacher when signup form is submitted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,20 +9,31 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "abcdef"
   end
 
+  
   get '/' do
     erb :welcome
   end
+
 
   get '/login' do 
     erb :login
   end
 
+
   get '/signup' do
     erb :signup
   end
 
+
   post '/signup' do
-  
+    if params["password"]] == "" || params["email"] == "" || params["name"] == ""
+      redirect to '/signup'
+    else
+      @teacher = Teacher.create(params)
+      @session = session
+      @session[:user_id] = @teacher.id
+      redirect to '/lessons'
+    end
   end
 
 
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :email, :password, :username  
+  validates_presence_of :email, :password, :name  
   has_many :courses
   has_many :lessons, through: :courses
 end
