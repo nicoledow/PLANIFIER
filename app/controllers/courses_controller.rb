1ba45601fd26890dc5114a6d054dd5c51f35b4a4
Nicole Dow <nikki.dow71@gmail.com>
2019-09-10 16:43:34 -0400

    refactored edit button - now is link to /courses/:id/edit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,12 +8,7 @@ class CoursesController < ApplicationController
   end
 
 
-  get '/courses/new' do
-    @teacher = current_user
-    erb :'/courses/new'
-  end
-
-#could use an error message here
+  #could use an error message here
   post '/courses' do
     @course = Course.new(name: params["name"], teacher_id: current_user.id)
 
@@ -26,12 +21,28 @@ class CoursesController < ApplicationController
   end
 
 
+  get '/courses/new' do
+    @teacher = current_user
+    erb :'/courses/new'
+  end
+
+
 #could use an error message here -- if lesson doesn't exist (invalid URL)
   get '/courses/:id' do
     verify_logged_in
     @course = Course.find_by_id(params["id"].to_i)
     @lessons = Lesson.all.select {|lesson| lesson.course_id == @course.id}
     erb :'/courses/show'
+  end
+
+  patch '/courses/:id' do
+    binding.pry
+    @course = Course.find_by_id(params["id"])
+  end
+
+
+  delete '/courses/:id' do
+
   end
 
 
