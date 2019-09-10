04360b434d5f50305d930fcbd2ebda020662bbeb
Nicole Dow <nikki.dow71@gmail.com>
2019-09-10 13:20:14 -0400

    refactored to use verify_logged_in helper method

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,23 +1,12 @@
 class TeachersController < ApplicationController
 
     get '/teachers' do
-    #   if logged_in?
-    #     erb :'/teachers/index'
-    #   else
-    #     redirect to '/login'
-    #   end
       verify_logged_in
       erb :'/teachers/index'
     end
 
 
     get '/teachers/:id' do
-    #   if logged_in?
-    #     @teacher = Teacher.find_by_id(params["id"])
-    #     erb :'/teachers/show'
-    #   else
-    #     redirect to '/login'
-    #   end
       verify_logged_in
       @teacher = Teacher.find_by_id(params["id"])
       erb :'/teachers/show'
