9a76901dfdfabfbff1f3ae1575d484d482454ea8
Nicole Dow <nikki.dow71@gmail.com>
2019-09-10 11:47:22 -0400

    added @teacher instance var to transfer to show page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,8 +1,9 @@
 class TeachersController < ApplicationController
 
     get '/teachers/:id' do
-
+      @teacher = Teacher.find_by_id(params["id"])
+      erb :'/teachers/show'
     end
 
-    
+
 end
