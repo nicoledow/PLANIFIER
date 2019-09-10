12af4afebb312197a4059b3a6a00e55b12eb8a1a
Nicole Dow <nikki.dow71@gmail.com>
2019-09-10 11:20:10 -0400

    refactored welcome message for current teacher

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
 
     get '/lessons' do 
       if logged_in?
-        @teacher = Teacher.find_by_id(session[:user_id])
+        @teacher = current_user
         @teacher_first_name = @teacher.name.split(" ").first
         @lessons = Lesson.all
         erb :'/lessons/show_all'
