8bf119cce35453433018f3d3f41decf440b8a503
Nicole Dow <nikki.dow71@gmail.com>
2019-09-09 21:43:35 -0400

    created links to individual lesson show pages

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,4 +5,10 @@ class CoursesController < ApplicationController
     erb :'/courses/index'
   end
 
+  get '/courses/:id' do
+    @course = Course.find_by_id(params["id"].to_i)
+    @lessons = Lesson.all.select {|lesson| lesson.course_id == @course.id}
+    erb :'/courses/show'
+  end
+
 end
