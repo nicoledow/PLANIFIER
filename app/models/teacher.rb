58a31d8f17b211dbd78cfd8827edf94dd7da050c
Nicole Dow <nikki.dow71@gmail.com>
2019-09-13 13:23:07 -0400

    created SavedLesson class

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,5 +6,5 @@ class Teacher < ActiveRecord::Base
           
   has_many :courses
   has_many :lessons, through: :courses
-  
+  has_many :lessons_saved, through: :saved_lessons, class_name: "Lesson"
 end
